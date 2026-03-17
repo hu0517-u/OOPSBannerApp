84cecc51af8c2a2953de25d6ad74f9ba1a2a9ce8
hu0517-u <hu0517@srmist.edu.in.com>
2026-03-17 14:06:03 +0530

    Added UC2

---
+++ OOPSBannerApp_uc2.java
@@ -0,0 +1,18 @@
+public class OOPSBannerApp_uc2 {
+
+    public static void main(String[] args) {
+
+        // Each println represents one row (7 rows total)
+
+        System.out.println("   ***    " + "   ***    " + "******   " + "   ****  ");
+        System.out.println(" **   **  " + " **   **  " + "**    ** " + " **      ");
+        System.out.println("**     ** " + "**     ** " + "**     **" + "**       ");
+        System.out.println("**     ** " + "**     ** " + "**    ** " + " **      ");
+        System.out.println("**     ** " + "**     ** " + "******   " + "   ***   ");
+        System.out.println("**     ** " + "**     ** " + "**       " + "      ** ");
+	    System.out.println("**     ** " + "**     ** " + "**       " + "       **");
+        System.out.println(" **   **  " + " **   **  " + "**       " + "      ** ");
+		System.out.println("   ***    " + "   ***    " + "**       " + " *****   ");
+    }
+    }
+}
