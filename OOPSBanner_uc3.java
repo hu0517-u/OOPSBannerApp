61c8160345d4eaec6c8508891343ccfc2722ac16
hu0517-u <hu0517@srmist.edu.in.com>
2026-03-18 15:09:25 +0530

    Added UC3

---
+++ OOPSBanner_uc3.java
@@ -0,0 +1,11 @@
+public class OOPSBanner_uc3 {
+    public static void main(String[] args) {
+        System.out.println(String.join("", "  *****  ", "  *****  ", " ********", " ********"));
+        System.out.println(String.join("", " **   ** ", " **   ** ", " **    **", "**       "));
+        System.out.println(String.join("", "**     **", "**     **", " **    **", "**       "));
+        System.out.println(String.join("", "**     **", "**     **", " ********", " ********"));
+        System.out.println(String.join("", "**     **", "**     **", " **      ", "       **"));
+        System.out.println(String.join("", " **   ** ", " **   ** ", " **      ", "       **"));
+        System.out.println(String.join("", "  *****  ", "  *****  ", " **      ", "******** "));
+    }
+}
